3210d16dda2e1c40ff07a4255958163297d97579
y_benjo <oreno_bukiwo_sitteruka@yahoo.co.jp>
2010-06-24 16:40:28 +0900

    README追記

--- src/bigraph.h
+++ src/bigraph.h
@@ -12,13 +12,25 @@
 #include <cmath>
 #include <fstream>
 #include <string.h>
+#include <tr1/unordered_map>
 
 using namespace std;
+using namespace std::tr1;
 typedef unsigned int uint;
+typedef pair<uint, uint> key;
+typedef set<uint> list;
+typedef map<key, double> d_hash;
+typedef unordered_map<uint, list> node_list;
 
 class BiGraph{
 public:
   BiGraph(){};
+protected:
+  node_list v_1;
+  node_list v_2;
+  map<key, uint> weight;
+  d_hash score_1_2;
+  d_hash score_2_1;
 };
 
 #endif //__class__BiGraph__
